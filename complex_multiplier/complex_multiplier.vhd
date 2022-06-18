496072f52ba9c9d09b022339543f0bf93858c951
Andrey Smolyakov <andreismolyakow@gmail.com>
2022-06-19 00:32:56 +0300

    Relax requirement for complex_multiplier ports
    Widths of the multiplied numbers' components can now no be equal.

--- complex_multiplier/complex_multiplier.vhd
+++ complex_multiplier/complex_multiplier.vhd
@@ -24,10 +24,13 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity complex_multiplier is
     port (
-        clk        : in  std_ulogic;
-        a_re, a_im : in  signed;
-        b_re, b_im : in  signed;
-        c_re, c_im : out signed
+        clk  : in  std_ulogic;
+        a_re : in  signed;
+        a_im : in  signed;
+        b_re : in  signed;
+        b_im : in  signed;
+        c_re : out signed;
+        c_im : out signed
     );
 
     attribute LATENCY : natural;
@@ -35,21 +38,44 @@ entity complex_multiplier is
 
     -- attribute use_dsp : string;
     -- attribute use_dsp of complex_multiplier : entity is "yes";
-    
+
 end complex_multiplier;
 
 architecture rtl of complex_multiplier is
-    
-    constant MULT_WIDTH : positive := a_re'length + b_re'length;
-    constant RES_WIDTH  : positive := MULT_WIDTH + 1;
 
-    signal a_re_d1, a_re_d2, a_im_d1, a_im_d2 : signed(a_re'length-1 downto 0);
-    signal b_re_d1, b_re_d2, b_im_d1, b_im_d2 : signed(b_re'length-1 downto 0);
-    signal are_bre, aim_bim, are_bim, aim_bre : signed(MULT_WIDTH-1  downto 0);
-    signal c_re_s, c_im_s                     : signed(RES_WIDTH-1   downto 0);
+    -- For determining the result's width
+    function max(a, b : natural) return natural is
+    begin
+        if a > b then return a;
+        else return b;
+        end if;
+    end;
+
+    -- Internal multiplications widths
+    constant RE_RE_WIDTH : natural := a_re'length + b_re'length;
+    constant IM_IM_WIDTH : natural := a_im'length + b_im'length;
+    constant RE_IM_WIDTH : natural := a_re'length + b_im'length;
+    constant IM_RE_WIDTH : natural := a_im'length + b_re'length;
+
+    -- Result's components widths
+    constant RE_WIDTH : natural := max(RE_RE_WIDTH, IM_IM_WIDTH) + 1;
+    constant IM_WIDTH : natural := max(RE_IM_WIDTH, IM_RE_WIDTH) + 1;
+
+    signal a_re_d1, a_re_d2 : signed(a_re'length-1 downto 0);
+    signal a_im_d1, a_im_d2 : signed(a_im'length-1 downto 0);
+    signal b_re_d1, b_re_d2 : signed(b_re'length-1 downto 0);
+    signal b_im_d1, b_im_d2 : signed(b_im'length-1 downto 0);
+
+    signal are_bre : signed(RE_RE_WIDTH-1 downto 0);
+    signal aim_bim : signed(IM_IM_WIDTH-1 downto 0);
+    signal are_bim : signed(RE_IM_WIDTH-1 downto 0);
+    signal aim_bre : signed(IM_RE_WIDTH-1 downto 0);
+
+    signal c_re_s : signed(RE_WIDTH-1 downto 0);
+    signal c_im_s : signed(IM_WIDTH-1 downto 0);
 
 begin
-    
+
     -- 1st DSP48
     are_bre_calc_prcs : process(clk)
     begin
@@ -59,7 +85,7 @@ begin
             are_bre <= a_re_d1*b_re_d1;
         end if;
     end process are_bre_calc_prcs;
-    
+
     -- 2nd DSP48
     real_calc_prcs : process(clk)
     begin
@@ -67,10 +93,10 @@ begin
             a_im_d1 <= a_im;
             b_im_d1 <= b_im;
             aim_bim <= a_im_d1 * b_im_d1;
-            c_re_s  <= resize(are_bre,RES_WIDTH) - resize(aim_bim,RES_WIDTH);
+            c_re_s  <= resize(are_bre, RE_WIDTH) - aim_bim;
         end if;
     end process real_calc_prcs;
-    
+
     -- 3d DSP48
     are_bim_calc_prcs : process(clk)
     begin
@@ -80,7 +106,7 @@ begin
             are_bim <= a_re_d2*b_im_d2;
         end if;
     end process are_bim_calc_prcs;
-    
+
     -- 4th DSP48
     imag_calc_prcs : process(clk)
     begin
@@ -88,7 +114,7 @@ begin
             a_im_d2 <= a_im;
             b_re_d2 <= b_re;
             aim_bre <= a_im_d2 * b_re_d2;
-            c_im_s  <= resize(are_bim,RES_WIDTH) + resize(aim_bre,RES_WIDTH);
+            c_im_s  <= resize(are_bim, IM_WIDTH) + aim_bre;
         end if;
     end process imag_calc_prcs;
 
